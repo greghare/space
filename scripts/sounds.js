cc06077e9c88eb61036d9913d140baaa056571c0
miimario <mii@cfl.rr.com>
2015-12-15 09:47:15 -0500

    Removed unecessary files

--- scripts/sounds.js
+++ scripts/sounds.js
@@ -6,20 +6,6 @@ var basePath = "sounds/";
 var rocket;
 
 function loadSounds() {
-    // rocket = new Audio(basePath + "rocket2.mp3");
-    rocket = new Howl({
-        urls: [basePath + 'rocket4.mp3'],
-        loop: true,
-        onplay: function() {
-            this.isPlaying = true;
-        },
-        onend: function() {
-            this.isPlaying = false;
-        },
-        onpause: function() {
-            this.isPlaying = false;
-        }
-    });
 
     music = new Howl({
         urls: [basePath + 'FromUnderAVision.mp3'],
